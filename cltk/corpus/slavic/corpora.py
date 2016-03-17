0e5be7ef27d25946494e942b9f2f3e2d858d6865
Alex K <alex.lumir@gmail.com>
2016-03-17 22:58:11 +0300

    St. Petersburg corpus of hagiographic texts added for Slavic

---
+++ cltk/corpus/slavic/corpora.py
@@ -0,0 +1,12 @@
+"""Old Church Slavic language corpora available for download or loading locally.
+All remote corpora hosted by github on the cltk organization account, eg:
+'http://github.com/cltk' + name
+"""
+
+LATIN_CORPORA = [
+    {'encoding': 'utf-8',
+     'markup': 'xml',
+     'location': 'remote',
+     'type': 'text',
+     'name': 'slavic_text_scht'}
+]
